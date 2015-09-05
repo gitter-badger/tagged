ca8066201c012b3ba711a8f1939c78ab7e1e39d0
Thorsten Hans <thorsten.hans@gmail.com>
2015-09-05 18:37:41 +0200

    added spinner

--- frontend/www/app/browse/browse.js
+++ frontend/www/app/browse/browse.js
@@ -1,30 +1,40 @@
 (function(jQuery, $){
 
-    function BrowseController($scope, $cordovaGeolocation){
+    function BrowseController($scope, $cordovaGeolocation, store){
         var viewModel = this;
-        viewModel.address = null;
+        viewModel.location = null;
         viewModel.result = "";
+        viewModel.isLocating = false;
 
         viewModel.useMyLocation = function(){
+          viewModel.isLocating = true;
           var posOptions = {timeout: 10000, enableHighAccuracy: false};
           $cordovaGeolocation
           .getCurrentPosition(posOptions)
           .then(function (position) {
-
               var geocoder = new google.maps.Geocoder;
               geocoder.geocode({'location': {lat: position.coords.latitude, lng: position.coords.longitude}},
                 function(results, status) {
                   if(results.length > 0){
-                    viewModel.address = results[0].formatted_address;
+                    viewModel.location = {
+                      title: results[0].formatted_address,
+                      lat: position.coords.latitude,
+                      lng: position.coords.longitude
+                    };
+                    store.set('lastLocation', viewModel.location);
+                    viewModel.isLocating = false;
                     $scope.$apply();
                   }
                 });
-              var lat  = position.coords.latitude
-              var long = position.coords.longitude
           }, function(err) {
-            // error
+              viewModel.isLocating = false;
+              $scope.$apply();
           });
         };
+
+        $scope.$on('$ionicView.enter', function(){
+          viewModel.location = store.get('lastLocation');
+        });
     }
 
     angular.module('tagged').controller('browseController', BrowseController);
